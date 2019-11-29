88b549c71c458c7289473d0db2776435c3dc7870
Anton Kanugalawattage <antondilon@gmail.com>
2019-11-29 05:33:08 -0500

    json and app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render() {
     return(
       <BrowserRouter>
-        {/* <Route exact path='/' component={Homepage}/> */}
+        <Route exact path='/' component={Homepage}/>
       </BrowserRouter>
     )
   }
